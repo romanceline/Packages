3ae20f9f41a29e178ac84b7829117d29cdeee3ee
romanceline <48318414+romanceline@users.noreply.github.com>
2021-01-12 22:30:56 +0100

    test
    Testing Rt Chart

--- RtChart/R/RtChart.R
+++ RtChart/R/RtChart.R
@@ -1,7 +1,7 @@
 #' R chart function
 #' This function allows you to create a chart displaying the reproduction number over time
 #' @param country - specify the country to draw the chart for
-
+#' RtChart()
 
 RtChart <- function(ctr){
 
