7674766441d7f82215751083c253223092ebefa7
romanceline <48318414+romanceline@users.noreply.github.com>
2021-01-19 01:14:41 +0100

    Update on package structure

--- SpaguettiPlot/R/SpaguettiPlot.R
+++ SpaguettiPlot/R/SpaguettiPlot.R
@@ -1,53 +1,65 @@
 #' SpaguettiPlot function
-#' This function will highlight one variable (NewDeats, NewCases, CasesIncidence, DeathsIncidence) for one country against other countries
+#' This function will highlight one variable (NewDeaths, NewCases, CasesIncidence, DeathsIncidence) for one country against other countries
 #' @param Country - Country to highlight
-#' @param Variable - Variable can be NewCases,NewDeaths,TotalCases,TotalDeaths, DailyRateCases, DailyRateDeaths, CumulativeRateCases,CumulativeRateDeaths
+#' @param Variable - Variable can be NewCases,NewDeaths,NewCases_Rate,NewDeaths_Rate (will be developed further with other variables)
 #' @param AllOrSelection - Use 'All' if you want to compare the selected country to all other countries in EURO Region, use 'c(CountryA,CountryB,CountryC,...)' if you want to compare to some countries only
-#'
 #' @param color - Color to use to highlight the country Ex: '#000000'
 #' @export
 
 SpaguettiPlot<-function (Country,Variable,AllOrSelection,ListCountriesToCompare,col){
-  Population<-Population %>% mutate(ADM0NAME=str_to_title(ADM0NAME)) %>% select(ADM0NAME,UNPOP2019)
+  AllCountries<-Population$ADM0NAME
 
-  Dataset<-GetEpiData() %>% mutate(ADM0NAME=str_to_title(ADM0NAME)) %>%
-    left_join(Population, by='ADM0NAME') %>%
-    mutate(DailyRateCases=NewCases/UNPOP2019*100000,
-           DailyRateDeaths=NewDeaths/UNPOP2019*100000,
-           CumulativeRateCases=TotalCases/UNPOP2019*1000000,
-           CumulativeRateDeaths=TotalDeaths/UNPOP2019*1000000)
+  Dataset<-BuildExtendedEpiDataset(Country)
 
-  Variable<-sym(Variable)
-
-  if(AllOrSelection=="All"){
-    ComparativeDataset<-Dataset
+  GlobalDataset<-data.frame()
+  if (AllOrSelection=='All'){
+    ListCountriesToCompare<-AllCountries
   }
 
-  if(AllOrSelection=="Selection"){
-    ComparativeDataset <- Dataset %>% filter (ADM0NAME %in% ListCountriesToCompare)
+  for (ctr in ListCountriesToCompare){
+    GlobalDataset_<-BuildExtendedEpiDataset(ctr)
+    GlobalDataset<-bind_rows(GlobalDataset_,GlobalDataset)
   }
+
+  if (Variable=='NewCases'){
+    Dataset_<-Dataset %>% select(ADM0NAME,DateReport,y_value=Spline_3DaysAverageCases)
+    GlobalDataset<-GlobalDataset %>% select(ADM0NAME,DateReport,y_value=Spline_3DaysAverageCases)
+  }
+
+  if (Variable=='NewDeaths'){
+    Dataset_<-Dataset %>% select(ADM0NAME,DateReport,y_value=Spline_3DaysAverageDeaths)
+    GlobalDataset<-GlobalDataset %>% select(ADM0NAME,DateReport,y_value=Spline_3DaysAverageDeaths)
+  }
+
+  if (Variable=='NewCases_Rate'){
+    Dataset_<-Dataset %>% select(ADM0NAME,DateReport,y_value=Spline_3DaysAverageCases_Rate)
+    GlobalDataset<-GlobalDataset %>% select(ADM0NAME,DateReport,y_value=Spline_3DaysAverageCases_Rate)
+  }
+
+  if (Variable=='NewDeaths_Rate'){
+    Dataset_<-Dataset %>% select(ADM0NAME,DateReport,y_value=Spline_3DaysAverageDeaths_Rate)
+    GlobalDataset<-GlobalDataset %>% select(ADM0NAME,DateReport,y_value=Spline_3DaysAverageDeaths_Rate)
+  }
+
+
+
 
   if (grepl("Cases", Variable))
   {if (Variable=="NewCases") {labelY<-"Daily number of cases"}
-  else if (Variable=="TotalCases") {labelY<-"Total number of cases"}
-  else if (Variable=="DailyRateCases") {labelY<-"Daily rate of cases (per 100.000)"}
-  else if (Variable=="CumulativeRateCases") {labelY<-"Cumulative rate of cases (per 100.000)"}}
+  if(Variable=="NewCases_Rate") {labelY<-"Daily rate of cases (per 100.000)"}}
 
-  else if (grepl("Deaths", Variable))
+  if (grepl("Deaths", Variable))
   {if (Variable=="NewDeaths") {labelY<-"Daily number of deaths"}
-  else if (Variable=="TotalDeaths") {labelY<-"Total number of deaths"}
-  else if (Variable=="DailyRateDeaths") {labelY<-"Daily rate of deaths (per one million)"}
-  else if (Variable=="CumulativeRateDeaths") {labelY<-"Cumulative rate of deaths (per one million)"}}
+  if (Variable=="NewDeaths_Rate") {labelY<-"Daily rate of deaths (per one million)"}}
 
 
-  CountryDataset<-Dataset %>% filter (ADM0NAME==Country)
 
   plot<-ggplot()+
-    geom_line(data=ComparativeDataset,aes(x=DateReport,y=!!Variable,group=ADM0NAME),color="grey")+
-    geom_line(data=CountryDataset,
-              aes(x=DateReport,y=!!Variable),
+    geom_line(data=GlobalDataset,aes(x=DateReport,y=y_value,group=ADM0NAME),color="grey")+
+    geom_line(data=Dataset_,
+              aes(x=DateReport,y=y_value),
               color=col,size=0.8)+
     labs(x="Date",y=labelY)+
     theme_minimal()
-  return(list(plot=plot))
+  return(plot=plot)
 }
